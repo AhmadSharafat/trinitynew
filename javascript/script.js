554fb8b5206ff1ad4270e6b7517c449085f93315
AhmadSharafat <112656643+AhmadSharafat@users.noreply.github.com>
2024-02-17 23:06:11 +0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,45 @@
+function openCity(evt, cityName) { // Declare all variables
+    var i,
+        tabcontent,
+        tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+$(document).ready(function () {
+    var owl = $("#slider-carousel");
+    owl.owlCarousel({
+        items: 4,
+        itemsDesktop: [
+            1000, 4
+        ],
+        itemsDesktopSmall: [
+            900, 2
+        ],
+        itemsTablet: [
+            600, 1
+        ],
+        itemsMobile: false,
+        pagination: false
+    });
+    $(".next").click(function () {
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function () {
+        owl.trigger('owl.prev');
+    })
+});
